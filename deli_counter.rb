b4ed5b82ddcad30b4da5feb4055e9c9dc827cdb2
Adham Muhammad <admuaq@gmail.com>
2018-07-10 07:08:34 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,4 +13,8 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving
+  if katz_deli.length == 0 
   puts "There is nobody waiting to be served!"
+  
+else
+  puts "Currently serving #{katz_deli.shift}"
