88438824fef9048407767f85a14320c6227236f8
Adham Muhammad <admuaq@gmail.com>
2018-07-10 07:07:11 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,3 +11,6 @@ def take_a_number(katz_deli, name)
   
   puts "Welcome #{name}. You are number #{katz_deli.length} in line."
 end
+
+def now_serving
+  puts "There is nobody waiting to be served!"
