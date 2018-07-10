58181970f50eb28d157cba8c055e9ff916725b7f
Adham Muhammad <admuaq@gmail.com>
2018-07-10 07:44:10 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -25,7 +25,7 @@ def take_a_number(katz_deli, name)
   else 
   katz_deli << name
   
-  puts "Welcome #{name}. You are number #{katz_deli.length} in line."
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
 def now_serving
