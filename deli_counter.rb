7af65ab1014613e8006b26db9d483394388b8c5d
Adham Muhammad <admuaq@gmail.com>
2018-07-10 07:49:49 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,9 +19,6 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
-  if katz_deli.length == 0 
-    puts "The line is currently empty."
-  else 
   katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
   end 
