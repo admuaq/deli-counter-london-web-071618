d6f205723b64449540acd5ead0bc5d1396bc4fa6
Adham Muhammad <admuaq@gmail.com>
2018-07-10 07:25:53 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,7 +9,7 @@ else
   message = "The line is currently: "
   i = 0
   while i < katz_deli.length
-    message+="#{i+1}.#{katz_deli[i]"
+    message+="#{i+1}"+"#{katz_deli[i]"
     i++
   end 
   puts message
