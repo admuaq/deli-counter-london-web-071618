bc1dd6d75523949f4efe5716867d3f6a50b8fce5
Adham Muhammad <admuaq@gmail.com>
2018-07-10 06:57:56 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,4 +4,7 @@ katz_deli = []
 def line
   puts "The line is currently empty."
 
-def take_a_number
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  
+  puts "Welcome #{name}. You are number #{katz_deli.lenght} in line."
